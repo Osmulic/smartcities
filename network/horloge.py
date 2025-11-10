0397556ce607cef7ed15700a7c54680053ab7a66
Osmulic <osman.225@hotmail.com>
2025-11-10 15:42:26 +0100

    Ajout de la synchronisation de l'heure et ajustement de l'angle de déplacement du servo, pas de 15 degré

--- network/horloge.py
+++ network/horloge.py
@@ -1,6 +1,24 @@
 from machine import Pin, PWM
 from time import sleep
+import time
 import network
+import ntptime
+
+def time_sync(timezone_offset_hours=0):
+    global local_time
+    try:
+        ntptime.settime()  # synchronise l'heure UTC
+        
+        # Appliquer le décalage horaire
+        t = time.localtime()
+        h = (t[3] + timezone_offset_hours) % 24
+        local_time = (h, t[4])
+
+        return local_time
+    except Exception as e:
+        print("Échec de la synchronisation de l'heure :", e)
+        return None
+
 
 # --- Configuration du servo ---
 servo = PWM(Pin(20))
@@ -12,6 +30,9 @@ password = "MDP"
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(ssid, password)
+
+time_sync(1)  # UTC+1 pour la Belgique)
+print(f"Heure après synchronisation {local_time[0]} heures {local_time[1]} minutes")
 
 def set_angle(angle):
     # Limite les angles dans une plage sûre
@@ -27,7 +48,7 @@ try:
     if wlan.isconnected():
         print("Wi-Fi connecté, mouvement du servo...")
         print("Adresse IP:", wlan.ifconfig()[0])
-        for a in range(0, 181, 90):
+        for a in range(0, 181, 15):
             set_angle(a)
             print("Angle:", a)
             sleep(1)
